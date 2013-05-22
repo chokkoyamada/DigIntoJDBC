8d40b5707cf4da64238515ecda53b16556eff13b
Naoyuki Yamada <yamadanaoyuki@mac.com>
2013-05-23 08:40:46 +0900

    project file cleanup

--- src/main/java/com/kirishikistudios/continuousops/digintojdbc/chapter2.java
+++ src/main/java/com/kirishikistudios/continuousops/digintojdbc/chapter2.java
@@ -1,15 +1,33 @@
 package com.kirishikistudios.continuousops.digintojdbc;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Chapter2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         request.setAttribute("subject", "JDBC");
+        Context context;
+        try {
+            context = new InitialContext();
+            DataSource dataSource = (DataSource)context.lookup("java:comp/env/jdbc/continuousops");
+            Connection connection = dataSource.getConnection();
+            request.setAttribute("message", "Successfully connected to database.");
+            connection.close();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         this.getServletContext().getRequestDispatcher("/chapter2/index.jsp").forward(request, response);
     }
 }
--- src/main/webapp/chapter2/index.jsp
+++ src/main/webapp/chapter2/index.jsp
@@ -1,14 +1,8 @@
 <%@ page contentType="text/html; charsert=UTF-8" %>
-<%@ page import="javax.naming.Context, javax.naming.InitialContext, javax.sql.DataSource, java.sql.Connection " %>
 <!DOCTYPE html>
 <html>
 <body>
 <h2>Hello ${requestScope['subject']}!</h2>
-<%
-    Context context = new InitialContext();
-    DataSource dataSource = (DataSource)context.lookup("java:comp/env/jdbc/continuousops");
-    Connection connection = dataSource.getConnection();
-    connection.close();
-%>
+<p>${requestScope['message']}</p>
 </body>
 </html>
